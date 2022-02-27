b9efa9ffd92d7da14154f0f2dd637c15612343eb
Johan794 <johan.ricardo1104@gmail.com>
2022-02-27 15:45:16 -0500

    update chess board and adding style

--- src/index.js
+++ src/index.js
@@ -8,6 +8,103 @@ import './index.css';
 
 
 
+class Board extends React.Component{
+  render() {
+      return(
+          <div className="board">
+              <h1>Chess view</h1>
+            <table id="tablero">
+                  <tr>
+                  <td>&#9820;</td>
+                  <td>&#9822;</td>
+                  <td>&#9821;</td>
+                  <td>&#9819;</td>
+                  <td>&#9818;</td>
+                  <td>&#9821;</td>
+                  <td>&#9822;</td>
+                  <td>&#9820;</td>
+              </tr>
+              <tr>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+              </tr>
+              <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+              </tr>
+              <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+              </tr>
+              <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+              </tr>
+              <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+              </tr>
+              <tr>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+                  <td>&#9823;</td>
+              </tr>
+              <tr>
+                  <td>&#9820;</td>
+                  <td>&#9822;</td>
+                  <td>&#9821;</td>
+                  <td>&#9819;</td>
+                  <td>&#9818;</td>
+                  <td>&#9821;</td>
+                  <td>&#9822;</td>
+                  <td>&#9820;</td>
+              </tr>
+          </table>
+          </div>
+      );
+  }
+}
+
+
+
+
+
+
 
 
 
@@ -17,7 +114,7 @@ import './index.css';
 
 
 ReactDOM.render(
-
+ <Board />,
   document.getElementById('root')
 );
 
